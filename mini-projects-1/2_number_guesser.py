598d3e1d439ff2bdb0297522ac563381c80b7692
pixiedust07 <sharonshaji127001@gmail.com>
2024-05-17 11:37:19 +0530

    Add guidence to guess number faster

--- mini-projects-1/2_number_guesser.py
+++ mini-projects-1/2_number_guesser.py
@@ -1,12 +1,16 @@
 #generate a random number
 #track the times it takes to guess correctly
 
+#make user choose the limit
+#narrow down guess range when it's wrong.
+
 import _helper as hp
 from _helper import Colors
 import random
 
 def main():
-    number = random.randint(0,10)
+    max_range = int(input("Enter the range of number to be guessed : "))
+    number = random.randint(0,max_range+1)
     tries = 1
     limit = 5
 
@@ -19,8 +23,11 @@ def main():
                 print(f"Enter your Guess [{tries}] : {guess} {hp.ct(Colors.GREEN, "is correct.")}")
                 print(hp.ct(Colors.YELLOW, f"you took {tries} tries\n"))
                 break;
-            elif(guess != number):
-                print(f"Enter your Guess [{tries}] : {guess} {hp.ct(Colors.RED, "try again!")}")
+            elif(guess >= number):
+                print(f"Enter your Guess [{tries}] : {guess} {hp.ct(Colors.RED, "try lower!")}")
+                tries+=1
+            elif(guess <= number):
+                print(f"Enter your Guess [{tries}] : {guess} {hp.ct(Colors.RED, "try higher!")}")
                 tries+=1
         except ValueError:
             print(hp.ct(Colors.RED, "Enter numbers only!"))
